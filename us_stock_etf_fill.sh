9d4fc3253343cb58c8086b10e9b8f5dbf8a079ae
이상민 <isangmin@MacBook-Air-3.local>
2025-01-22 12:22:16 +0900

    Update: 2025-01-22 12:22:16

---
+++ us_stock_etf_fill.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define the script to execute
+SCRIPT_NAME="us_stock_fill.py"
+
+# Navigate to the script directory (adjust as needed)
+SCRIPT_DIR="/home/ec2-user/quant_invest/KIS_quant"  
+cd "$SCRIPT_DIR" || exit
+
+LOG_FILE="${SCRIPT_NAME%}_$(date '+%Y-%m-%d_%H-%M-%S').log"
+
+# Start the script in the background
+echo "Starting $SCRIPT_NAME ..."
+nohup python3 -u "$SCRIPT_NAME" > "$LOG_FILE" &
